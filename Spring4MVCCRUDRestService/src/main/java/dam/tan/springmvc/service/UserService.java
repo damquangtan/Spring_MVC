1aae0b0c5c8c21a5428323952fe3d2a239550782
damquangtan <damquangtan92@gmail.com>
2018-03-07 18:03:51 +0700

    spring 4 mvc crud with rest web service from spring4mvc_crud branch

---
+++ Spring4MVCCRUDRestService/src/main/java/dam/tan/springmvc/service/UserService.java
@@ -0,0 +1,16 @@
+package dam.tan.springmvc.service;
+
+import java.util.List;
+
+import dam.tan.springmvc.model.User;
+
+public interface UserService {
+	User findById(long id);
+	User findByName(String name);
+	void saveUser(User user);
+	void updateUser(User user);
+	void deleteUserById(long id);
+	List<User> findAllUsers();
+	void deleteAllUsers();
+	public boolean isUserExist(User user);
+}
